7dd5a014d50b047e97e85272067554d057809959
jcabrera <julian.cabreras@gmail.com>
2021-04-06 17:03:46 -0500

    Service and logout completed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -16,14 +17,22 @@ export class LoginComponent implements OnInit {
   //Angular.giveMeRouter
   //Dependency Injection
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private hardcodedAuthenticationService: HardcodedAuthenticationService
+  ) {}
 
   ngOnInit(): void {}
 
   handleLogin() {
     //console.log(this.username)
-    if (this.username === 'in28minutes' && this.password === 'dummy') {
-      //Redicrect to welcome page
+    if (
+      this.hardcodedAuthenticationService.authenticate(
+        this.username,
+        this.password
+      )
+    ) {
+      // Redirect to welcome page
       this.router.navigate(['welcome', this.username]);
       this.invalidLogin = false;
     } else {
